e73aada233ae298d5c9750ae0258c589c66811f1
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-04-03 23:22:41 +0530

    added decrment action

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,15 +1,20 @@
-import { createReducer, on } from "@ngrx/store";
-import { increment } from "./counter.actions";
+import { Action, createReducer, on } from "@ngrx/store";
+import { decrement, increment } from "./counter.actions";
+//import { CounterActions, INCREMENT, IncrementAction } from "./counter.actions";
 
 const initialState = 0;
 
 export const counterReducer = createReducer(
     initialState,
-    on(increment, (state, action) => state + action.value)
+    on(increment, (state, action) => state + action.value),
+    on(decrement, (state, action) => state - action.value)
 );
 
 
-// export function counterReducer (state = initialState){
+// export function counterReducer (state = initialState, action: CounterActions | Action){
+//     if(action.type === INCREMENT){
+//         return state + (action as IncrementAction).value;
+//     }
 //     return state;
 // }
 
